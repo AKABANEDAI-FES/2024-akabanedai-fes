e974c0754d257fdf1e0157621944a57c57ce4844
yu7400ki <yu7400ki@gmail.com>
2024-09-30 23:39:37 +0900

    metadataBase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import { Header } from "@/components/site/header";
 import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
+	metadataBase: process.env.METADATA_BASE
+		? new URL(process.env.METADATA_BASE)
+		: undefined,
 	title: "赤羽台祭",
 	description:
 		"東洋大学赤羽台キャンパス大学祭「赤羽台祭」は、2024/11/02(土), 03(日)に開催！",
