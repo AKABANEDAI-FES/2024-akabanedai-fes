04b655bb8bebc8bc7c3a1fbf7b8fb7c96ecdb4e0
yu7400ki <yu7400ki@gmail.com>
2024-10-01 15:28:03 +0900

    optim hero

--- src/components/site/hero/hero.tsx
+++ src/components/site/hero/hero.tsx
@@ -1,16 +1,21 @@
-import hero from "@/assets/hero.png";
+import hero from "@/assets/hero.webp";
 import Image from "next/image";
 import styles from "./hero.module.css";
 
 export function Hero() {
 	return (
-		<div>
+		<div
+			className={styles.hero}
+			style={{
+				aspectRatio: `${hero.width}/${hero.height}`,
+			}}
+		>
 			<Image
-				className={styles.hero}
 				src={hero.src}
 				height={hero.height}
 				width={hero.width}
 				alt=""
+				loading="eager"
 			/>
 		</div>
 	);
