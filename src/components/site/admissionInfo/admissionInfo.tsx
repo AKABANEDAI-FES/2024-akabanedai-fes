2a218d07eb2abe411aaa29d25bd4a2d7b60cb527
1f10230002 <s1f102300021@iniad.org>
2024-10-08 23:42:19 +0900

    入場について完成

--- src/components/site/admissionInfo/admissionInfo.tsx
+++ src/components/site/admissionInfo/admissionInfo.tsx
@@ -1,14 +1,20 @@
 import { Section } from "../section";
+import styles from "./admissionInfo.module.css";
+import "./admissionInfo.module.css"
+import { Heading } from "../heading";
 
 export function AdmissionInfo() {
   return (
-    <Section>
-      <h2>入場について</h2>
-      <ul>
-        <li>事前予約やチケットは不要です</li>
-        <li>入場料は無料です。(※ 一部体験や飲食出店は有料です。)</li>
-        <li>・駐車場はございませんので、お車でのご来場はご遠慮ください。</li>
-      </ul>
+    <Section className={styles.section}>
+      <div className={styles.admmissionInfo}>
+       <Heading className={styles.heading}>入場について</Heading>
+
+        <ul className={styles.list}>
+          <li className={styles.listItem}>事前予約やチケットは不要です</li>
+          <li className={styles.listItem}>入場料は無料です。(※ 一部体験や飲食出店は有料です。)</li>
+          <li className={styles.listItem}>駐車場はございませんので、お車でのご来場はご遠慮ください。</li>
+        </ul>
+      </div>
     </Section>
   )
 }
