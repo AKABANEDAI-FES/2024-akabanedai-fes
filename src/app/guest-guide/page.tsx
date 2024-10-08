aa1326c3294c2bedf402850df6bc02ac17b8fa37
1f10230002 <s1f102300021@iniad.org>
2024-10-09 02:15:40 +0900

    困ったことがあったらのセクションが完成

--- src/app/guest-guide/page.tsx
+++ src/app/guest-guide/page.tsx
@@ -1,13 +1,13 @@
 // import { Access } from "@/components/site/access";
 import { AdmissionInfo } from "@/components/site/admissionInfo";
 import { Guidelines } from "@/components/site/guidelines";
-// import { HelpAndSupport } from "@/components/site/helpAndSupport";
+import { HelpAndSupport } from "@/components/site/helpAndSupport";
 export default function Page() {
   return (
     <>
       <AdmissionInfo />
       <Guidelines />
-      {/* <HelpAndSupport/> */}
+      <HelpAndSupport/>
     </>
   );
 }
