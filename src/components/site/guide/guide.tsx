26fcad4d15b95db34e3c7c886bea318344e8947a
1f10230002 <s1f102300021@iniad.org>
2024-10-09 01:42:42 +0900

    キャンパス内での注意事項完成

--- src/components/site/guide/guide.tsx
+++ src/components/site/guide/guide.tsx
@@ -1,26 +1,28 @@
 import { Section } from "../section";
 import styles from "./guide.module.css";
-import { Heading } from "../heading";
+
 
 export type Props = {
-	headingText: string;
-  listItem: string[];
-  
+	headingText:  React.ReactNode;
+  listItem?: string[];
+  children?: React.ReactNode;
 };
 
-export function Guide({ headingText,listItem}:Props) {
+export function Guide({ headingText,listItem,children}:Props) {
   return (
     <Section className={styles.section}>
-      <div className={styles.admmissionInfo}>
-        <Heading className={styles.heading}>{headingText}</Heading>
-
-        <ul className={styles.list}>
-          {listItem.map((item, index) => (
+      <div className={styles.guideInfo}>
+        <h2 className={styles.heading}>{headingText}</h2>
+        <div className={styles.contents}>
+        <ul>
+          {listItem&& listItem.map((item, index) => (
             <li key={index} className={styles.listItem}>
               {item}
             </li>
           ))}
         </ul>
+        {children}
+      </div>
       </div>
     </Section>
   );
