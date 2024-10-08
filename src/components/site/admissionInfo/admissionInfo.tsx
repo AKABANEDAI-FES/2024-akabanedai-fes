ac9b0dd9bd6cfc896282ad45b12de16d9de316d1
1f10230002 <s1f102300021@iniad.org>
2024-10-08 14:06:15 +0900

    ご来場の皆様へのファイルを作成

---
+++ src/components/site/admissionInfo/admissionInfo.tsx
@@ -0,0 +1,14 @@
+import { Section } from "../section";
+
+export function AdmissionInfo() {
+  return (
+    <Section>
+      <h2>入場について</h2>
+      <ul>
+        <li>事前予約やチケットは不要です</li>
+        <li>入場料は無料です。(※ 一部体験や飲食出店は有料です。)</li>
+        <li>・駐車場はございませんので、お車でのご来場はご遠慮ください。</li>
+      </ul>
+    </Section>
+  )
+}
