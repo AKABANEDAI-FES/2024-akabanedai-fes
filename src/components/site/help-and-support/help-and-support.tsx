8468624bda8ba231ff5f613c1471831a463c3d16
1f10230002 <s1f102300021@iniad.org>
2024-10-10 00:46:08 +0900

    コードの修正5

--- src/components/site/help-and-support/help-and-support.tsx
+++ src/components/site/help-and-support/help-and-support.tsx
@@ -7,21 +7,23 @@ export function HelpAndSupport() {
 
 		return (
 			<Guide headingText={headingText}>
-				<p className={styles.info}>
-					困ったときはインフォメーションセンターへ
-				</p>
+				<p className={styles.info}>困ったときはインフォメーションセンターへ</p>
 				<p className={styles.info}>
 					インフォメーションセンターは迷子や落とし物、その他分からないことがあった時などのためにある相談所です。
 				</p>
 				<p className={styles.infoTroubles}>
 					イベントの開催場所がわからない
-					<br />一緒に来た人とはぐれてしまった
-					<br />持ち物を無くしてしまった・・・
+					<br />
+					一緒に来た人とはぐれてしまった
+					<br />
+					持ち物を無くしてしまった・・・
 				</p>
 				<p className={styles.info}>
 					など様々なトラブルに対応します。
-					<br />お困りの際は、インフォメーションセンターにお気軽においでください。
-					<br />また、会場内をスタッフが巡回しています。ジャンパーを着用した学生にお気軽にお声がけください。
+					<br />
+					お困りの際は、インフォメーションセンターにお気軽においでください。
+					<br />
+					また、会場内をスタッフが巡回しています。ジャンパーを着用した学生にお気軽にお声がけください。
 				</p>
 			</Guide>
 		);
