1145151ad855976b19e2791e761c31a5a34295a4
1f10230002 <s1f102300021@iniad.org>
2024-10-10 16:06:14 +0900

    最初の文と目次を追加

--- src/app/guest-guide/page.tsx
+++ src/app/guest-guide/page.tsx
@@ -1,15 +1,46 @@
+import Link from "next/link";
 import { AdmissionInfo } from "@/components/site/admissionInfo";
 import { Guidelines } from "@/components/site/guidelines";
 import { Heading } from "@/components/site/heading";
 import { HelpAndSupport } from "@/components/site/help-and-support";
+import styles from "./guest-guide.module.css";
+
+function Summary() {
+	return (
+		<ul className={styles.list}>
+			<li className={styles.listItem}>
+				<Link href="#addmissionInfo" className={styles.link}>入退場について</Link>
+			</li>
+			<li className={styles.listItem}>
+				<Link href="#guidelines" className={styles.link}>キャンパス内での注意事項</Link>
+			</li>
+			<li className={styles.listItem}>
+				<Link href="#helpAndSupport" className={styles.link}>困ったことがあったら</Link>
+			</li>
+		</ul>
+	)
+}
+
+
 
 export default function Page() {
 	return (
 		<>
 			<Heading>ご来場の皆様へ</Heading>
-			<AdmissionInfo />
-			<Guidelines />
-			<HelpAndSupport />
+			<div className={styles.request}>
+					<p className={styles.requestItem}>赤羽台祭へご来場の方は、以下の注意事項をお守りくださいますようお願いいたします。</p>
+					<p className={styles.requestItem}>※掲載内容は順次更新いたしますので最新情報を必ずご確認の上、ご参加ください。</p>
+			</div>
+			<Summary />
+			<div id="addmissionInfo" className={styles.linkScroll}>
+				<AdmissionInfo />
+			</div>
+			<div id="guidelines" className={styles.linkScroll}>
+				<Guidelines />
+			</div>
+			<div id="helpAndSupport" className={styles.linkScroll}>
+				<HelpAndSupport />
+			</div>
 		</>
 	);
 }
