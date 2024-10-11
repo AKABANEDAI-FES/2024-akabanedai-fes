bfc29cefe12b1b776ead1c75063e8c7f940252b1
yu7400ki <yu7400ki@gmail.com>
2024-10-11 22:16:43 +0900

    program list

---
+++ src/components/site/program-list/program-list.stories.tsx
@@ -0,0 +1,28 @@
+import type { Program } from "@/types/program";
+import type { Meta, StoryObj } from "@storybook/react";
+import { ProgramList } from "./program-list";
+
+const meta = {
+	title: "Site/ProgramList",
+	component: ProgramList,
+	tags: ["autodocs"],
+} satisfies Meta<typeof ProgramList>;
+
+export default meta;
+
+type Story = StoryObj<typeof ProgramList>;
+
+const data = Array.from({ length: 20 }, (_, i) => ({
+	title: `Program ${i + 1}`,
+	logo: "https://placehold.jp/150x150.png",
+	organization: "Organization",
+	placement: "Placement",
+	tags: Array.from({ length: i }, (_, j) => `Tag ${j + 1}`),
+	description: "Description",
+})) satisfies Program[];
+
+export const Default: Story = {
+	args: {
+		programs: data,
+	},
+};
