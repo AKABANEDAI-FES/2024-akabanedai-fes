1d8b731f003b078b11f3978b2317e12fd7b2680d
1f10220137 <s1f102201379@iniad.org>
2024-10-13 23:39:15 +0900

    fix error

--- src/app/contest/page.tsx
+++ src/app/contest/page.tsx
@@ -1,10 +1,10 @@
-import './contest.css'
+import "./contest.css";
 
 export default function Contest() {
 	return (
 		<div className="contest-container">
 			<nav className="breadcrumb">
-				<a href="#">企画一覧</a> &gt; 企画コンテスト
+				<a href="/">企画一覧</a> &gt; 企画コンテスト
 			</nav>
 			<h1>企画コンテスト</h1>
 			<h2>企画コンテストとは</h2>
@@ -13,9 +13,12 @@ export default function Contest() {
 			</p>
 			<h2>投票期間</h2>
 			<p>
-				赤羽台祭の期間中、キャンパス内に設置されている投票所で投票できます。<br />
-				11月2日 12:00~18:00<br />11月3日 12:00~15:00<br />
-				※ スケジュールは都合により一部変更になる可能性があります。
+				赤羽台祭の期間中、キャンパス内に設置されている投票所で投票できます。
+				<br />
+				11月2日 12:00~18:00
+				<br />
+				11月3日 12:00~15:00
+				<br />※ スケジュールは都合により一部変更になる可能性があります。
 			</p>
 			<h2>投票方法</h2>
 			<p>
@@ -29,24 +32,37 @@ export default function Contest() {
 			</div>
 			<h2>結果発表</h2>
 			<p>
-				表彰式は 11月3日 17:30 より、WELLBメインステージにて行います。さらに、表彰された企画はこちらの特設ページでも掲載いたします。ぜひご確認ください。
+				表彰式は 11月3日 17:30
+				より、WELLBメインステージにて行います。さらに、表彰された企画はこちらの特設ページでも掲載いたします。ぜひご確認ください。
 			</p>
 			<h2>表彰企画</h2>
 			<p>
-				以下のように部門に分けて表彰します。<br /><br />
-				<strong>グランプリ</strong><br />
-				赤羽台祭に参加している、大学生主体のすべての企画の中から、1位、2位、3位を決定します。<br />
-				<strong>チーム実習部門</strong><br />
-				INIADの学部3年生による「情報連携チーム実習」の団体企画を対象とします。<br />
-				<strong>食品部門</strong><br />
-				屋台企画などの、食品や飲料を販売する企画を対象とします。<br />
-				<strong>ステージ部門</strong><br />
-				WELLBメインステージおよびINIADホールで公演を行う企画を対象とします。<br />
-				<strong>サークル部門</strong><br />
+				以下のように部門に分けて表彰します。
+				<br />
+				<br />
+				<strong>グランプリ</strong>
+				<br />
+				赤羽台祭に参加している、大学生主体のすべての企画の中から、1位、2位、3位を決定します。
+				<br />
+				<strong>チーム実習部門</strong>
+				<br />
+				INIADの学部3年生による「情報連携チーム実習」の団体企画を対象とします。
+				<br />
+				<strong>食品部門</strong>
+				<br />
+				屋台企画などの、食品や飲料を販売する企画を対象とします。
+				<br />
+				<strong>ステージ部門</strong>
+				<br />
+				WELLBメインステージおよびINIADホールで公演を行う企画を対象とします。
+				<br />
+				<strong>サークル部門</strong>
+				<br />
 				サークル団体による企画を対象とします。
 			</p>
-			<button className="back-button">企画一覧に戻る</button>
+			<button type="button" className="back-button">
+				企画一覧に戻る
+			</button>
 		</div>
 	);
 }
-
