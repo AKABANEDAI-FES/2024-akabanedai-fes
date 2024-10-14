7c10645b64ab0510a26051ec4b54abe410a92e9c
yu7400ki <yu7400ki@gmail.com>
2024-10-14 13:51:43 +0900

    improve title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,14 +12,20 @@ export const metadata: Metadata = {
 		? new URL(process.env.METADATA_BASE)
 		: undefined,
 	manifest: path.join(nextConfig.basePath || "", "/manifest.webmanifest"),
-	title: "赤羽台祭",
+	title: {
+		template: "%s | 赤羽台祭",
+		default: "赤羽台祭",
+	},
 	description:
 		"東洋大学赤羽台キャンパス大学祭「赤羽台祭」は、2024/11/02(土), 03(日)に開催！",
 	keywords: ["東洋大学", "大学祭", "学園祭", "赤羽台祭", "赤羽"],
 	openGraph: {
 		url: "https://akabanedai-fes.com/",
 		type: "website",
-		title: "赤羽台祭 [東洋大学赤羽台キャンパス大学祭]",
+		title: {
+			template: "%s | 赤羽台祭 [東洋大学赤羽台キャンパス大学祭]",
+			default: "赤羽台祭 [東洋大学赤羽台キャンパス大学祭]",
+		},
 		siteName: "赤羽台祭",
 	},
 	twitter: {
