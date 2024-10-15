2beb2fea75d3d592f9e0f5bb0e20e0d8dd6f7275
1f10230002 <s1f102300021@iniad.org>
2024-10-15 10:02:07 +0900

    logoセクションのcssを追加

--- src/components/site/logo/logo.tsx
+++ src/components/site/logo/logo.tsx
@@ -1,12 +1,26 @@
+import Image from "next/image";
+import { Section } from "../section";
 import styles from "./logo.module.css";
 
 export function Logo() {
 	return (
-		<div className={styles.logo}>
-			<h1>今年のロゴ「鯉の滝登り」</h1>
-			<p>
-				ロゴはテーマである挑戦からイメージした、鯉の滝登りがモチーフになっています。炎のように燃え盛る鯉には、赤羽台祭が炎のように盛り上がり続け、消えないで欲しいという思いが込められています。
-			</p>
-		</div>
+		<Section className={styles.logo}>
+			<Image
+				src="/pwa-512x512.png"
+				alt=""
+				width={400}
+				height={400}
+				className={styles.image}
+			/>
+			<h2 className={styles.title}>
+				<span>今年のロゴ</span>
+				<span>「鯉の滝登り」</span>
+			</h2>
+			<div className={styles.content}>
+				<p>
+					ロゴはテーマである挑戦からイメージした、鯉の滝登りがモチーフになっています。炎のように燃え盛る鯉には、赤羽台祭が炎のように盛り上がり続け、消えないで欲しいという思いが込められています。
+				</p>
+			</div>
+		</Section>
 	);
 }
