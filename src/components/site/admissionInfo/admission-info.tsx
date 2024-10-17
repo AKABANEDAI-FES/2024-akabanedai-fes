6221cad165888d646e4e067a7508e23d0f95f40d
1f10230002 <s1f102300021@iniad.org>
2024-10-17 11:28:06 +0900

    文面の添削

--- src/components/site/admissionInfo/admission-info.tsx
+++ src/components/site/admissionInfo/admission-info.tsx
@@ -1,11 +1,11 @@
 import { Guide } from "../guide";
 
 export function AdmissionInfo() {
-	const headingText = "入場について";
+	const headingText = "入退場について";
 	const admissionInfo: string[] = [
-		"事前予約やチケットは不要です",
+		"事前予約やチケットは不要です。",
 		"入場料は無料です。(※ 一部体験や飲食出店は有料です。)",
-		"駐車場はございませんので、お車でのご来場はご遠慮ください。",
+		"駐車場のご用意はございません。また駐輪場は数に限りがございます。ご来場の際は、できる限り公共交通機関をご利用ください。",
 	];
 	return <Guide headingText={headingText} listItem={admissionInfo} />;
 }
