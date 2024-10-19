fcfa66d94eb3ce43568382638dc067af9dbb61ad
Yuki Natori <104719412+yu7400ki@users.noreply.github.com>
2024-10-19 23:40:15 +0900

    協賛企業 (#38)
    * sponsor log
    
    * sponsor json
    
    * sponsor component
    
    * 新しいタブで開く
    
    * fix sponsor style

---
+++ src/utils/sponsor.ts
@@ -0,0 +1,10 @@
+import sponsor from "@/assets/sponsor.json";
+import type { Sponsor } from "@/types/sponsor";
+
+export function getSponsors(): Sponsor[] {
+	return sponsor.map((sponsor) => ({
+		name: sponsor.企業名,
+		url: sponsor.url,
+		logo: `/sponsor/${sponsor.ロゴ}`,
+	}));
+}
