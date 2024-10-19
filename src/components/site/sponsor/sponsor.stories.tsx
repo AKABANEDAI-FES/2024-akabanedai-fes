045643caef9d7a317eb16277f2f99253d363eda9
Yuki Natori <104719412+yu7400ki@users.noreply.github.com>
2024-10-20 01:03:33 +0900

    協賛追加 (#42)
    * ロゴなし
    
    * fix utils
    
    * ロゴなし協賛追加
    
    * fix

--- src/components/site/sponsor/sponsor.stories.tsx
+++ src/components/site/sponsor/sponsor.stories.tsx
@@ -1,17 +1,17 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { Sponsor } from "./sponsor";
+import { Sponsors } from "./sponsor";
 
 const meta = {
 	title: "Site/Sponsor",
-	component: Sponsor,
+	component: Sponsors,
 	tags: ["autodocs"],
 	parameters: {
 		layout: "fullscreen",
 	},
-} as Meta<typeof Sponsor>;
+} as Meta<typeof Sponsors>;
 
 export default meta;
 
-type Story = StoryObj<typeof Sponsor>;
+type Story = StoryObj<typeof Sponsors>;
 
 export const Default: Story = {};
--- src/components/site/sponsor/sponsor.tsx
+++ src/components/site/sponsor/sponsor.tsx
@@ -1,3 +1,4 @@
+import type { Sponsor } from "@/types/sponsor";
 import { getSponsors } from "@/utils/sponsor";
 import { buildPath } from "@/utils/url";
 import Image from "next/image";
@@ -5,30 +6,67 @@ import { Heading } from "../heading";
 import { Section } from "../section";
 import styles from "./sponsor.module.css";
 
-export function Sponsor() {
+export function Sponsors() {
 	const sponsors = getSponsors();
+
+	const sponsorsWithLogos = sponsors.filter(
+		(sponsor): sponsor is Omit<Sponsor, "logo"> & { logo: string } =>
+			sponsor.logo !== null,
+	);
+	const sponsorsWithoutLogos = sponsors.filter(
+		(sponsor): sponsor is Omit<Sponsor, "logo"> & { logo: null } =>
+			sponsor.logo === null,
+	);
 
 	return (
 		<Section className={styles.container}>
 			<Heading>協賛企業さま</Heading>
 			<div className={styles.sponsor}>
-				{sponsors.map((sponsor) => (
-					<a
-						key={sponsor.name}
-						href={sponsor.url}
-						className={styles.link}
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<Image
-							className={styles.image}
-							src={buildPath(sponsor.logo)}
-							alt={sponsor.name}
-							width={300}
-							height={200}
-						/>
-					</a>
-				))}
+				<div className={styles["with-logo"]}>
+					{sponsorsWithLogos.map((sponsor) => {
+						const image = (
+							<Image
+								key={sponsor.name}
+								className={styles.image}
+								src={buildPath(sponsor.logo)}
+								alt={sponsor.name}
+								width={300}
+								height={200}
+							/>
+						);
+						return sponsor.url !== null ? (
+							<a
+								key={sponsor.name}
+								className={styles.link}
+								href={sponsor.url}
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								{image}
+							</a>
+						) : (
+							<div key={sponsor.name} className={styles.link}>
+								{image}
+							</div>
+						);
+					})}
+				</div>
+				<div className={styles["without-logo"]}>
+					{sponsorsWithoutLogos.map((sponsor) =>
+						sponsor.url !== null ? (
+							<a
+								key={sponsor.name}
+								href={sponsor.url}
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								{sponsor.name}
+							</a>
+						) : (
+							<span key={sponsor.name}>{sponsor.name}</span>
+						),
+					)}
+				</div>
 			</div>
 		</Section>
 	);
