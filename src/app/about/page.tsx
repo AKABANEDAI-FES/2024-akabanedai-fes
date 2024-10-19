fbb13f3bf62275fe5eb0a6dd1ae9e8c3abf59a1a
yu7400ki <yu7400ki@gmail.com>
2024-10-19 18:44:27 +0900

    metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,6 +3,14 @@ import { Greeting } from "@/components/site/greeting/";
 import { Logo } from "@/components/site/logo";
 import { Mascot } from "@/components/site/mascot/";
 import { Slideshow } from "@/components/site/slideshow";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "赤羽台祭について",
+	openGraph: {
+		title: "赤羽台祭について",
+	},
+};
 
 export default function AboutPage() {
 	return (
