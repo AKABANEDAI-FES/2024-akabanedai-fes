a56e8818b476e6e93db2c8ae02f83cacc473ba5d
mikoto2726 <s1f102201379@iniad.org>
2024-10-20 20:24:22 +0900

    classnameに修正

--- src/components/site/contest/voting-period/page.tsx
+++ src/components/site/contest/voting-period/page.tsx
@@ -9,8 +9,8 @@ export function VotingPeriod() {
 				赤羽台祭の期間中、キャンパス内に設置されている投票所で投票できます。
 				<br />
 				<br />
-				<span class="p-date2">11月2日 12:00~18:00</span>
-				<span class="p-date2">11月3日 12:00~15:00</span>
+				<span classname="p-date2">11月2日 12:00~18:00</span>
+				<span classname="p-date2">11月3日 12:00~15:00</span>
 				<br />※ スケジュールは都合により一部変更になる可能性があります。
 			</p>
 		</div>
