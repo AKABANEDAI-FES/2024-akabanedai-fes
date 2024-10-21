aed73ae64b43edbda4927522009e05058efc82d9
s1F102302528-bob <136312529+s1F102302528-bob@users.noreply.github.com>
2024-10-21 15:42:51 +0900

    書式修正

--- src/components/site/merchandise/merchandise.tsx
+++ src/components/site/merchandise/merchandise.tsx
@@ -1,12 +1,12 @@
 import styles from "./merchandise.module.css";
 
 type Props = {
-	title: string
-	description: string
-	smallText: string
-	price: string
-	imgLink: string
-}
+	title: string;
+	description: string;
+	smallText: string;
+	price: string;
+	imgLink: string;
+};
 
 export function Merchandise(props: Props) {
 	return (
@@ -16,12 +16,12 @@ export function Merchandise(props: Props) {
 				<div className={styles.descriptionContainer}>
 					<p className={styles.description}>{props.description}</p>
 					<p className={styles.smallText}>{props.smallText}</p>
-					<span className={styles.space}/>
+					<span className={styles.space} />
 					<h3 className={styles.price}>{props.price}</h3>
 					<p className={styles.smallText}>円</p>
 				</div>
-				<img src={props.imgLink} alt="" className={styles.merchPhoto}/>
+				<img src={props.imgLink} alt="" className={styles.merchPhoto} />
 			</div>
 		</div>
-	)
-};
+	);
+}
