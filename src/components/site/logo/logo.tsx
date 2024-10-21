844dde6af9e0ddd654a948b4d65c6dfaaf6d9a36
s1f10240150 <s1F102401506@iniad.org>
2024-10-21 15:20:10 +0900

    Update logo.tsx

--- src/components/site/logo/logo.tsx
+++ src/components/site/logo/logo.tsx
@@ -1,12 +1,23 @@
 import styles from "./logo.module.css";
+import Image from "next/image";
 
 export function Logo() {
 	return (
-		<div className={styles.logo}>
-			<h1>今年のロゴ「鯉の滝登り」</h1>
-			<p>
-				ロゴはテーマである挑戦からイメージした、鯉の滝登りがモチーフになっています。炎のように燃え盛る鯉には、赤羽台祭が炎のように盛り上がり続け、消えないで欲しいという思いが込められています。
-			</p>
+		<div className={styles.encount}>
+			<Image
+				src="/encount.png"
+				alt=""
+				width={800}
+				height={600}
+				className={styles.image}
+			/>
+			<div className={styles.textWrapper}>
+				<h2 className={styles.title}>ENCOUNT!</h2>
+				<p className={styles.description}>
+					「ENCOUNT!」は今年の赤羽台祭のテーマである「挑戦」から着想を得ており、新しい技術(未知)への挑戦をイメージしたテーマです。ENCOUNTは遭遇、出会いという意味を持つ和製ゲーム用語であり、挑戦というフレーズから連想したRPGの要素を取り入れています。また昨年の「ENchant!」に寄せた名称は、前年からの飛躍を象徴する意味も込められています。
+				</p>
+			</div>
 		</div>
 	);
 }
+
