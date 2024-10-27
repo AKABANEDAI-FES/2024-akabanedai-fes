37ffd456bdcc1879c2f6bf3d8d1153e9ee7b53d7
potepote1818 <136304473+potepote1818@users.noreply.github.com>
2024-10-27 20:55:25 +0900

    公式グッズのページにmapを追加 (#62)
    * 公式グッズのページにmapを追加
    
    * トリミングしたマップに変換

--- src/app/official-merch/page.tsx
+++ src/app/official-merch/page.tsx
@@ -1,9 +1,11 @@
 import akukey from "@/assets/akukey.png";
 import ballpointPen from "@/assets/ballpoint_pen.jpg";
 import clearFile from "@/assets/clear_file.png";
+import goodsMap from "@/assets/goods-map.png";
 import handTowel from "@/assets/hand_towel.png";
 import postcard from "@/assets/postcard.png";
 import { Merchandise } from "@/components/site/merchandise";
+import Image from "next/image";
 import styles from "../guest-guide/guest-guide.module.css";
 
 export default function OfficialMerch() {
@@ -15,6 +17,18 @@ export default function OfficialMerch() {
 					<p className={styles.requestItem}>
 						東洋大学川越キャンパスの大学祭実行委員会キャラクターと赤羽台祭キャラクターのコラボオリジナルグッズです。各キャンパスでしか手に入らない限定品となってます！！
 					</p>
+				</div>
+				<div className={styles.description}>
+					<Image
+						src={goodsMap}
+						alt=""
+						width={goodsMap.width}
+						height={goodsMap.height}
+						loading="eager"
+						priority={true}
+						className={styles.map}
+					/>
+					<p>※公式グッズ販売はすべて対面となっております。</p>
 				</div>
 			</div>
 			<Merchandise
