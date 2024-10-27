800e6866b6bae2876a39f9d6719741ab14562ea0
yu7400ki <yu7400ki@gmail.com>
2024-10-27 19:08:49 +0900

    button component

---
+++ src/components/ui/button/button.tsx
@@ -0,0 +1,27 @@
+import { ark } from "@ark-ui/react";
+import clsx from "clsx";
+import type { ComponentProps } from "react";
+import styles from "./button.module.css";
+
+const variant = {
+	size: {
+		sm: styles.sm,
+		md: styles.md,
+		lg: styles.lg,
+		xl: styles.xl,
+	},
+};
+
+export type Props = ComponentProps<typeof ark.button> & {
+	size?: keyof typeof variant.size;
+};
+
+export function Button({ className, size = "md", ...props }: Props) {
+	return (
+		<ark.button
+			className={clsx(styles.btn, variant.size[size], className)}
+			data-size={size}
+			{...props}
+		/>
+	);
+}
