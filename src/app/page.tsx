51ca5a8749e92a09bc1a54bffd4f98418ab637db
1f10230002 <s1f102300021@iniad.org>
2024-10-28 01:41:53 +0900

    パンフレットのurlを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import { Access } from "@/components/site/access";
 import { Challenge } from "@/components/site/challenge";
+import Link from "next/link";
 import { Hero } from "@/components/site/hero";
 import { Sponsors } from "@/components/site/sponsor";
 import { Topic } from "@/components/site/topic/topic";
+import { buildPath } from "@/utils/url";
 
 export default function Home() {
 	return (
@@ -11,6 +13,7 @@ export default function Home() {
 			<Challenge />
 			<Topic />
 			<Access />
+			<Link href={buildPath("8thpamphlet.pdf")} target="_blank">パンフレット</Link>
 			<Sponsors />
 		</>
 	);
