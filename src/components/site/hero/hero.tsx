59cbe8314ea2546e2252a082b448938d20ce31fc
yu7400ki <yu7400ki@gmail.com>
2024-10-27 17:11:10 +0900

    画像差し替え

--- src/components/site/hero/hero.tsx
+++ src/components/site/hero/hero.tsx
@@ -1,22 +1,25 @@
-import hero from "@/assets/hero.webp";
-import Image from "next/image";
+import firstviewPc from "@/assets/firstview_pc.png";
+import firstviewSp from "@/assets/firstview_sp.png";
 import styles from "./hero.module.css";
 
 export function Hero() {
 	return (
-		<div
-			className={styles.hero}
-			style={{
-				aspectRatio: `${hero.width}/${hero.height}`,
-			}}
-		>
-			<Image
-				src={hero.src}
-				height={hero.height}
-				width={hero.width}
-				alt=""
-				loading="eager"
-			/>
+		<div className={styles.hero}>
+			<picture>
+				<source
+					srcSet={firstviewPc.src}
+					media="(min-width: 768px)"
+					width={firstviewPc.width}
+					height={firstviewPc.height}
+				/>
+				<img
+					src={firstviewSp.src}
+					alt="赤羽台祭 2024年11月2日（土）・3日（日）、10:00から18:00"
+					loading="eager"
+					width={firstviewSp.width}
+					height={firstviewSp.height}
+				/>
+			</picture>
 		</div>
 	);
 }
