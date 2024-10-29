9b02311a32a0ab2bdf859fb1a7a913c1a366f1a0
mikoto2726 <s1f102201379@iniad.org>
2024-10-29 10:55:52 +0900

    フォントサイズを変更

--- src/components/site/contest/award/page.tsx
+++ src/components/site/contest/award/page.tsx
@@ -9,31 +9,31 @@ export function Award() {
 				以下のように部門に分けて表彰します。
 				<br />
 				<br />
-				<strong>グランプリ</strong>
+				<strong className={styles.strong}>グランプリ</strong>
 				<br />
 				<span className={styles["p-award"]}>
 					赤羽台祭に参加する、大学生主体のすべての企画の中から、1位、2位、3位を決定します。
 				</span>
 				<br />
-				<strong>チーム実習部門</strong>
+				<strong className={styles.strong}>チーム実習部門</strong>
 				<br />
 				<span className={styles["p-award"]}>
 					INIAD学部3年生による「情報連携チーム実習」の団体企画を対象とした部門です。
 				</span>
 				<br />
-				<strong>実行委員会部門</strong>
+				<strong className={styles.strong}>実行委員会部門</strong>
 				<br />
 				<span className={styles["p-award"]}>
 					「赤羽台祭実行委員会」に所属する団体の企画を対象とした部門です。
 				</span>
 				<br />
-				<strong>研究室・ゼミ部門</strong>
+				<strong className={styles.strong}>研究室・ゼミ部門</strong>
 				<br />
 				<span className={styles["p-award"]}>
 					「ゼミ」または「研究室」に所属する団体の企画を対象とした部門です。
 				</span>
 				<br />
-				<strong>サークル部門</strong>
+				<strong className={styles.strong}>サークル部門</strong>
 				<br />
 				<span className={styles["p-award"]}>
 					「サークル」やその他の学生団体による企画を対象とした部門です。
