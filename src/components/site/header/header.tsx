54150f4b77dcd97a43eae542a01091e0f49aefb5
1f10230002 <s1f102300021@iniad.org>
2024-11-02 23:32:45 +0900

    アンケートを追加

--- src/components/site/header/header.tsx
+++ src/components/site/header/header.tsx
@@ -41,7 +41,9 @@ export function Header() {
 	return (
 		<header className={styles.header}>
 			{pathname === "/" ? (
-				<a href="https://forms.gle/y6nZgMDspT3LUAua6" className={styles.survey}>来場者アンケートはこちら</a>
+				<a href="https://forms.gle/y6nZgMDspT3LUAua6" className={styles.survey}>
+					来場者アンケートはこちら
+				</a>
 			) : (
 				<Link className={styles.logo} href="/" aria-label={site}>
 					{site}
